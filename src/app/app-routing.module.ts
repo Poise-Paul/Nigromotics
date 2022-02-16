65823d2b523d6f930b5661043b6d35dde255ee30
Nse-obong Paul <stargrades0@gmail.com>
2022-02-16 04:28:48 +0100

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [{path: 'dashboard', component: DashboardComponent}];
 
 @NgModule({
   imports: [
+    RouterModule,
     RouterModule.forRoot(routes),
   ],
   exports: [RouterModule],
